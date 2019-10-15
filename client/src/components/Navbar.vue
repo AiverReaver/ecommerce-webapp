c0d4a215f4970db1148d79063200b5ca179ac11a
Ashish Vagish <ashishvagish@outlook.in>
2019-10-15 16:25:47 +0530

    created basic frontend ui

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="ui secondary menu">
+    <router-link class="item" exact-active-class="active" to="/">Home</router-link>
+    <router-link
+      v-if="getLoggedInUserRole === 'restaurant'"
+      class="item"
+      exact-active-class="active"
+      to="/create"
+    >Create Restaurant</router-link>
+    <div class="right menu">
+      <a class="item" v-if="isLoggedIn" @click="logout">Logout</a>
+      <router-link v-if="!isLoggedIn" class="item" exact-active-class="active" to="/login">Login</router-link>
+      <div class="item" v-if="!isLoggedIn">
+        <router-link class="ui primary button" exact-active-class="active" to="/register">SignUp</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Navbar extends Vue {}
+</script>
+
+
